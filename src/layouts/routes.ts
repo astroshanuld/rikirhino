f8326cb700d1e830038220a6b2607bba1bc827a4
astroshanuld <iwirsa02@outlook.co.id>
2021-09-20 16:59:28 +0700

    fix: removed unused components

--- src/layouts/routes.ts
+++ src/layouts/routes.ts
@@ -1,15 +1,7 @@
 import dashboardRoutes from 'layouts/routes/admin/dashboard'
 import newsRoutes from 'layouts/routes/admin/news'
 import pagesRoutes from 'layouts/routes/admin/pages'
-import settingsRoutes from 'layouts/routes/admin/settings'
-import productRoutes from 'layouts/routes/admin/product'
 
-const globalRoutes = [].concat(
-  dashboardRoutes,
-  newsRoutes,
-  pagesRoutes,
-  settingsRoutes,
-  productRoutes,
-)
+const globalRoutes = [].concat(dashboardRoutes, newsRoutes, pagesRoutes)
 
 export default globalRoutes
